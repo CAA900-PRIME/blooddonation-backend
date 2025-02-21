0a22d448687148b9d99262094e66590550a7efc4
omar0ali <bajunaidomar@gmail.com>
2025-02-21 16:01:17 -0500

    feat: added cors to enable the backend to be accessed from a different domain name.

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 import config
 from api import register_apis # Import the function to register routes
 from models import db
+from flask_cors import CORS
 app = Flask(__name__)
 
+
 app.config.from_object(config.Config)
+
+# Enable CORS for all domains (Will need to modify later) only accept traffic from a specific port number
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+# i.ie : CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 db.init_app(app)  # Initialize the db with the app
 
