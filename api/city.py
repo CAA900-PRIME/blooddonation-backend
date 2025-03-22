9c5b9f641cab978684d0a771ca4b72f74de11ad7
omar0ali <bajunaidomar@gmail.com>
2025-03-22 02:44:17 -0400

    fix: using country name instead of code to get all the cities within a country

--- api/city.py
+++ api/city.py
@@ -4,7 +4,7 @@ from models import Cities, Countries
 city_api = Blueprint("city_api", __name__)
 
 @city_api.route("/get-cities", methods=["GET"])
-def get_countries():
+def get_cities():
     cities = Cities.query.all()
     listOfCities = []
     for city in cities:
@@ -16,9 +16,9 @@ def get_countries():
     return jsonify({"cities": listOfCities}), 200
 
 
-@city_api.route("/get-cities-with-code/<string:country_code>", methods=["GET"])
-def get_cities_by_country_code(country_code):
-    country = Countries.query.filter_by(code=country_code).first()
+@city_api.route("/get-cities-of-country/<string:country>", methods=["GET"])
+def get_cities_by_country_code(country):
+    country = Countries.query.filter_by(name=country).first()
     if not country:
         return jsonify({"error": "Country not found"}), 404
 
