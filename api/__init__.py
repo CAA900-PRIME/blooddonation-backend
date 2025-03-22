7bb2b2b005cd3c809fff414d2d6da1f26ede0410
Aniket-Banik <aniketbanik05@gmail.com>
2025-03-22 06:08:48 -0400

    NEW: Import 2FA blueprint

--- api/__init__.py
+++ api/__init__.py
@@ -2,9 +2,11 @@ from .auth import auth_api
 from .user import user_api
 from .events import events_api
 from .application import app_api
+from .two_factor import two_factor_bp  # NEW: Import 2FA blueprint
 
 def register_apis(app):
     app.register_blueprint(auth_api, url_prefix="/api/auth")
     app.register_blueprint(user_api, url_prefix="/api/users")
     app.register_blueprint(events_api, url_prefix="/api/events")
     app.register_blueprint(app_api, url_prefix="/api/app")
+
